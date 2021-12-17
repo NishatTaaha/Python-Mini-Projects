2c2a106762695984c1a09f709731020755d1c29e
Nishat Taaha <73392394+NishatTaaha@users.noreply.github.com>
2021-12-17 13:31:07 +0600

    Add files via upload

---
+++ Registration form.py
@@ -0,0 +1,59 @@
+from tkinter import *
+root=Tk()
+root.geometry('500x300')
+
+def getvals():
+    print('Accepted')
+
+##### Heading #####
+Label(root, text='Python Registration Form: ', font='arial 15 bold').grid(row=0, column=3)
+
+##### Field Name #####
+name= Label(root, text='Name ')
+phone= Label(root, text='Phone ')
+gender= Label(root, text='Gender: ')
+emergency= Label(root, text='Emergency ')
+paymentmood= Label(root, text='Payment Mood')
+
+
+##### Packing Fields #####
+name.grid(row=1, column=2)
+phone.grid(row=2, column=2)
+gender.grid(row=3, column=2)
+emergency.grid(row=4, column=2)
+paymentmood.grid(row=5, column=2)
+
+
+###### variabbles for sorting Data #####
+namevalue= StringVar
+phonevalue= StringVar
+gendervalue= StringVar
+emergencyvalue= StringVar
+paymentmoodvalue= StringVar
+checkvalue= IntVar
+
+
+##### Creating an entry feild #####
+nameentry= Entry(root, textvariable= namevalue)
+phoneentry= Entry(root, textvariable= phonevalue)
+genderentry= Entry(root, textvariable= gendervalue)
+emergencyentry= Entry(root, textvariable= emergencyvalue)
+paymentmoodentry= Entry(root, textvariable= paymentmoodvalue)
+
+
+##### Packing Entry Field #####
+nameentry.grid(row=1, column=3)
+phoneentry.grid(row=2, column=3)
+genderentry.grid(row=3, column=3)
+emergencyentry.grid(row=4, column=3)
+paymentmoodentry.grid(row=5, column=3)
+
+
+##### Checking Checkbox#####
+checkbtn= Checkbutton(text='Remember Me? ', variable= checkvalue)
+checkbtn.grid(row=6, column=3)
+
+##### Submit Buttons #####
+Button(text='Submit', command= getvals).grid(row=7, column=3)
+
+root.mainloop()
